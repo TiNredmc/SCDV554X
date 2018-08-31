648677947907cb1d126d6577c913e7d26c1145ba
ＴＩＮＬΞＴＨΛＸ <thipok17@hotmail.com>
2018-08-31 13:09:26 +0700

    Im back !

--- SCD554X_Example.ino
+++ SCD554X_Example.ino
@@ -1,12 +1,11 @@
 //This is example code for load data bit to SCD554X 4 digits vertical format 5x5 display from Osram.
 
 #include<SPI.h> //init. library for spi device .I will communicate with SCD554X via SPI bus interface .
-int Load= 10; //Load enable pin.
+int Load= 10; //Latch pin for the display 
 
 void setup(){
 pinMode(Load, OUTPUT);//set pin10 to output for switching "send bit" or "display data out"
 SPI.begin();
-SPI.setBitOrder(LSBFIRST);
 digitalWrite(Load, LOW);
 SPI.transfer(0xC0); // clear display
 digitalWrite(Load, HIGH);
@@ -22,6 +21,12 @@ digitalWrite(Load, HIGH);
 
 void loop(){
 digitalWrite(Load, LOW);
+SPI.transfer(0xA0);// select digit 0 for'up arrow'
+SPI.transfer(0x04);//00100
+SPI.transfer(0x2E);//01110
+SPI.transfer(0x55);//10101   
+SPI.transfer(0x64);//00100
+SPI.transfer(0x84);//00100
 SPI.transfer(0xA1);// select digit 1 for'H'
 SPI.transfer(0x11);//10001
 SPI.transfer(0x31);//10001
@@ -34,6 +39,12 @@ SPI.transfer(0x24);//00100
 SPI.transfer(0x44);//00100   I
 SPI.transfer(0x64);//00100
 SPI.transfer(0x8E);//01110
+SPI.transfer(0xA1);// select digit 3 for'down arrow'
+SPI.transfer(0x04);//00100
+SPI.transfer(0x24);//00100
+SPI.transfer(0x55);//10101   
+SPI.transfer(0x6E);//01110
+SPI.transfer(0x84);//00100
 digitalWrite(Load, HIGH);
 digitalWrite(Load, LOW);
 SPI.transfer(0xC0); // clear display
